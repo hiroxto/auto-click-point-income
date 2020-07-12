8fb431d2f1ba623a1cb3de5acd3dee94b6bcb630
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2020-07-12 18:35:21 +0900

    URLClicker クラスを実装

--- src/url-clicker.ts
+++ src/url-clicker.ts
@@ -1,7 +1,34 @@
+import { ChildProcess, exec } from 'child_process';
+
 export class URLClicker {
   urls: string[];
 
   constructor (urls: string[]) {
     this.urls = urls;
+  }
+
+  /**
+   * 広告 URL へのアクセスを実行する
+   */
+  execute (): ChildProcess {
+    return exec(this.buildCommand());
+  }
+
+  /**
+   * URL 一覧を表示する.
+   */
+  displayUrls (): string {
+    const joinedUrls = this.urls.join('\n');
+    console.log(joinedUrls);
+    return joinedUrls;
+  }
+
+  /**
+   * 実行するコマンドを作成する
+   */
+  private buildCommand (): string {
+    const joinedUrls = this.urls.map(u => `"${u}"`).join(' ');
+
+    return `curl -L ${joinedUrls} >> /dev/null`;
   }
 }
