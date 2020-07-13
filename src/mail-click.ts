3a9a9f77398e52aab0663d978b1f971ccabe1452
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2020-07-13 14:10:08 +0900

    MailClick 内のメソッドを整理

--- src/mail-click.ts
+++ src/mail-click.ts
@@ -22,8 +22,9 @@ export class MailClick {
 
       await mailMagazineUrls.map(async (mailMagazineUrl) => {
         console.log(mailMagazineUrl);
-        const urls = await this.openMailMagazines(mailMagazineUrl);
-        const clicker = new URLClicker(this.browser, urls);
+        const mailMagazinePage = await this.createNewPage(mailMagazineUrl);
+        const clickableUrls = await this.pickClickableUrls(mailMagazinePage);
+        const clicker = new URLClicker(this.browser, clickableUrls);
 
         if (clicker.isExecutable()) {
           clicker.displayUrls();
@@ -44,38 +45,43 @@ export class MailClick {
    * @param page
    */
   async getUnreadMailMagazineUrls (page: Page): Promise<string[]> {
-    return await page.$$eval<string[]>('.box_ad.notyet > td > a.txt_elli', (elements: HTMLAnchorElement[])  => {
-      return elements.map(element => element.href)
+    return await page.$$eval<string[]>('.box_ad.notyet > td > a.txt_elli', (elements: HTMLAnchorElement[]) => {
+      return elements.map(element => element.href);
     });
   }
 
-  async openMailMagazines (mailMagazineUrl: string): Promise<string[]> {
-    const page = await this.browser.newPage();
+  /**
+   * メールマガジンのページからクリック可能なURLを抽出する
+   *
+   * @param mailMagazinePage
+   */
+  async pickClickableUrls (mailMagazinePage: Page): Promise<string[]> {
+    return await mailMagazinePage.evaluate(() => {
+      const mailMagazineUrls = [];
+      const nodes = document.querySelectorAll<HTMLAnchorElement>('#mymail > pre.magagine_detail_txt > a');
+      nodes.forEach(node => {
+        const hrefValue = node.href;
 
-    try {
-      await page.setExtraHTTPHeaders(this.headers);
-      await page.goto(mailMagazineUrl);
-      const urls = await page.evaluate(() => {
-        const mailMagazineUrls = [];
-        const nodes = document.querySelectorAll<HTMLAnchorElement>('#mymail > pre.magagine_detail_txt > a');
-        nodes.forEach(node => {
-          const hrefValue = node.href;
-
-          if (hrefValue.match(/click_mail_magazine/)) {
-            mailMagazineUrls.push(hrefValue);
-          }
-        });
-
-        return mailMagazineUrls;
+        if (hrefValue.match(/click_mail_magazine/)) {
+          mailMagazineUrls.push(hrefValue);
+        }
       });
 
-      await page.close();
+      return mailMagazineUrls;
+    });
+  }
 
-      return urls;
-    } catch (e) {
-      console.log(e);
-      await page.close();
-      throw e;
-    }
+  /**
+   * HTTPヘッダーやタイムアウトの設定を済ませた新しいページを作成する
+   *
+   * @param url
+   */
+  async createNewPage (url: string): Promise<Page> {
+    const page = await this.browser.newPage();
+    await page.setExtraHTTPHeaders(this.headers);
+    await page.setDefaultNavigationTimeout(0);
+    await page.goto(url);
+
+    return page;
   }
 }
